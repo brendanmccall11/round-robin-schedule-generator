717c02ec7f4c55595fda82a081f8dfeb576ea331
Brendan McCall <brendanmccall3@gmail.com>
2025-08-27 20:38:47 -0600

    Finish main algorithm

--- src/main.py
+++ src/main.py
@@ -1,33 +1,54 @@
 import random
 
-def schedule_team(schedule_matrix, teams, team, team_index):
-    available_teams = teams[:]
-    available_teams.remove(team)
+class Team:
+    def __init__(self, name):
+        self.name = name
+        self.matchups = []
 
-    for opponent in range(len(schedule_matrix[team_index])):
-        random_opponent = random.choice(available_teams)
-        schedule_matrix[team_index][opponent] = random_opponent
-        available_teams.remove(random_opponent)
+    def get_name(self):
+        return self.name
 
+    def get_matchups(self):
+        return self.matchups
+
+    def add_matchup(self, opponent):
+        self.matchups.append(opponent)
+
+    def has_matchup(self, opponent):
+        for old_opponent in self.matchups:
+            if opponent == old_opponent:
+                return True
+        return False
 
 def __main__():
     teams = []
-    
-    num_teams = input("Enter number of teams: ")
 
-    for i in range(int(num_teams)):
-        user_input = input(f"Enter team {i+1}: ")
-        teams.append(user_input)
+    num_teams = int(input("Enter number of teams: "))
+    num_weeks = num_teams - 1
 
-    num_games_per_team = input("Input number of games per team: ")
+    for i in range(num_teams):
+        user_team = input(f"Enter team {i+1}: ")
+        new_team = Team(user_team)
+        teams.append(new_team)
 
-    schedule_matrix = [['.' for _ in range(int(num_games_per_team))] for _ in range(int(num_teams))]
+    for i in range(num_weeks):
+        available_teams = teams[:]
 
-    for team_index in range(len(teams)):
-        schedule_team(schedule_matrix, teams, teams[team_index], team_index)
+        print(f"WEEK {i + 1}:")
+        for j in range(int(num_teams / 2)):
+            random_away_team = random.choice(available_teams)
+            available_teams.remove(random_away_team)
 
-    for rows in schedule_matrix:
-        print(rows)
+            while True:
+                random_home_team = random.choice(available_teams)
+
+                if not random_away_team.has_matchup(random_home_team):
+                    available_teams.remove(random_home_team)
+                    random_away_team.add_matchup(random_home_team)
+                    random_home_team.add_matchup(random_away_team)
+                    print(f"Matchup {j + 1}: {random_away_team.get_name()} @ {random_home_team.get_name()}")
+                    break
+        print("----------------------------------")
 
 if __name__ == "__main__":
     __main__()
